1ebe7112ca510124a2fbae8c33e8fd5e157008a9
igordrozd <drozdigor06@gmail.com>
2022-11-23 16:55:51 +0300

    фывфыв

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/api/users/', async(req,res) => {
     if (record===null){
         req.body.password=bcrypt.hashSync(req.body.password, salt);
         const result = await User.create(req.body);
-        console.log(result);
+        
         
         
         const token = jwt.sign({name : result.name, password : result.password,id : result.id}, secret);
@@ -54,8 +54,7 @@ app.get('/api/users/', async(req,res)=>{
     const a = bcrypt.hashSync(req.body.password, salt);
     if (record!== null){    
         if (record.password !==a){
-            console.log(a)
-            console.log(record.password)
+            
             res.status(403).send('Erorr 403 (wrong password)');
         }
         else {
@@ -87,7 +86,7 @@ app.use((req, res, next) => {
 app.post('/api/tasks', async (req, res) => { 
     const token = req.headers.token 
     const user = jwt.verify(token, secret); 
-    console.log(user) 
+    
     const result = await Task.create({ 
         ...req.body, 
         userId: user.id 
@@ -96,7 +95,7 @@ app.post('/api/tasks', async (req, res) => {
  
     res.send(result); 
 });
-app.post('/api/docm/', async (req, res) => {
+app.post('/api/docments/', async (req, res) => {
     const result = await Document.create(req.body);
     res.send(result);
 });
@@ -147,7 +146,7 @@ app.delete('/api/users/:id', async (req, res) => {
 });
 
 
-app.delete('/api/docm/:id', async (req, res) => {
+app.delete('/api/docments/:id', async (req, res) => {
     const result = await Document.destroy({
         where: {
             id: req.params.id
