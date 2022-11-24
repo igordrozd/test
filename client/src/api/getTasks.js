d46fa76e255b2f430350d88ac4066b2751b4b149
igordrozd <drozdigor06@gmail.com>
2022-11-24 16:25:20 +0300

    asdasdasd

--- client/src/api/getTasks.js
+++ client/src/api/getTasks.js
@@ -1,5 +1,5 @@
 import sendRequest from '../utils/request';
 import port from '../port'
-export function getTasks() {
-    return sendRequest(`http:localhost:${port}/api/tasks/:id`, 'GET');
+export function getTasks(id) {
+    return sendRequest(`http:localhost:${port}/api/tasks/${id}`, 'GET');
 }
