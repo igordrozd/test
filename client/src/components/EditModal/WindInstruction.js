c6e7fb7c29991dcd93fbc1a0adac99195f1c318a
valeriy167 <fkn.vlr@gmail.com>
2022-11-26 15:55:12 +0300

    добавил поле инструкции, артем выход

---
+++ client/src/components/EditModal/WindInstruction.js
@@ -0,0 +1,23 @@
+import React from "react"
+import dayjs from 'dayjs';
+import { Form,  Input, TimePicker } from 'antd';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
+
+const defaultValue = dayjs('00:00', 'mm:ss')
+export const WindInstruction = () => (
+        <>
+            <Form.Item name="title">
+                <Input placeholder='Введите название'/>
+            </Form.Item>
+            <Form.Item name="time">
+                <TimePicker
+                    format="mm:ss"
+                    defaultOpenValue={defaultValue}
+                    style={{ width: `100%` }}
+                    defaultValue={defaultValue}
+                />
+            </Form.Item>
+        </>
+)
