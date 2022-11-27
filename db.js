7e85035d2b4e21cc5ba3dc2847c722ccbb417930
LiSteNite <lizastepina111@gmail.com>
2022-11-27 16:09:23 +0300

    Пароль

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('ptk', 'postgres', '1102', {
+module.exports = new Sequelize('ptk', 'postgres', 'Lizaazil228', {
     dialect: 'postgres',
     host: 'localhost',
     logging: false,
