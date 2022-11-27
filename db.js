563264e34c1bfd526800aceb83c4a338fc067870
igordrozd <drozdigor06@gmail.com>
2022-11-27 10:52:25 +0300

    фывфыв

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('ptk', 'postgres', 'postgres', {
+module.exports = new Sequelize('ptk', 'postgres', '1102', {
     dialect: 'postgres',
     host: 'localhost',
     logging: false,
